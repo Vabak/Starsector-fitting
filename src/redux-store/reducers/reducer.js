729a74754202ecfbc9b10f07ce51002d9ec7c624
Valeriy <highwaytowork@gmail.com>
2019-02-24 22:18:46 +0500

    Added redux, react-redux.

---
+++ src/redux-store/reducers/reducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    deck: [],
+    score: 0,
+    cardsRemain: 18,
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer
