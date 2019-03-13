27b3127323a088da969b1b786590babe6c502d84
Valeriy Bakharev <highwaytowork@gmail.com>
2019-03-14 04:35:19 +0500

    Why table does not styling?

--- src/components/ShipDataTable.js
+++ src/components/ShipDataTable.js
@@ -2,20 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledTable = styled.table`
-    width: 30%;
+    width: 40%;
+    colgroup {
+        border: 1px solid black;
+    }
 `;
 
-const DataTable = ( props ) => {
+const DataTable = (props) => {
     const data = props.data || {};
-    return ( 
+    return (
         <StyledTable>
-                            <thead>
+            <colgroup >
+                <col span="2" />
+                <col span="2" />
+                <col span="2" />
+            </colgroup>
+            <thead>
                 <tr>
                     <th colSpan="2">Logistical</th>
                     <th colSpan="2">Combat</th>
                     <th colSpan="2">Movement</th>
                 </tr>
-                </thead>
+            </thead>
             <tbody>
                 <tr>
                     <td>Supplies per month</td>
@@ -26,41 +34,51 @@ const DataTable = ( props ) => {
                     <td>{data.max_speed}</td>
                 </tr>
                 <tr>
-                    <td>Flux dissipation</td>
-                    <td>{data.flux_dissipation}</td>
+
                     <td>Hitpoints</td>
                     <td>{data.hitpoints}</td>
                     <td>Acceleration</td>
                     <td>{data.acceleration}</td>
                 </tr>
                 <tr>
+                    <td>Flux dissipation</td>
+                    <td>{data.flux_dissipation}</td>
                     <td>Fuel/light year </td>
                     <td>{data.fuel}/{data.fuel_ly}</td>
-                    <td>Flux</td>
-                    <td>{data.max_flux}</td>
+
                     <td>Deceleration</td>
                     <td>{data.deceleration}</td>
                 </tr>
                 <tr>
                     <td>Cargo</td>
                     <td>{data.cargo}</td>
-                    <td>Armor rating</td>
-                    <td>{data.armor_rating}</td>
+                    <td>Flux</td>
+                    <td>{data.max_flux}</td>
+
                     <td>Max turn rate</td>
                     <td>{data.max_turn_rate}</td>
                 </tr>
                 <tr>
                     <td>Mass</td>
                     <td>{data.mass}</td>
-                    <td>Shield type</td>
-                    <td>{data.shield_type}</td>
+                    <td>Armor rating</td>
+                    <td>{data.armor_rating}</td>
+
                 </tr>
                 <tr>
                     <td>Min/Max crew</td>
                     <td>{data.min_crew}/{data.max_crew}</td>
+                    <td>Shield type</td>
+                    <td>{data.shield_type}</td>
+
+                </tr>
+                <tr>
+                    <td />
+                    <td />
                     <td>Shield efficiency</td>
                     <td>{data.shield_efficiency}</td>
                 </tr>
+
                 <tr>
                     <td />
                     <td />
@@ -68,7 +86,7 @@ const DataTable = ( props ) => {
                     <td>{data.shield_arc}</td>
                 </tr>
                 <tr>
-                <td />
+                    <td />
                     <td />
                     <td>Shield upkeep</td>
                     <td>{data.shield_upkeep}</td>
@@ -78,5 +96,5 @@ const DataTable = ( props ) => {
     );
 }
 
- 
+
 export default DataTable;
